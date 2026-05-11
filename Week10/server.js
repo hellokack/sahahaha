ebe32a245dc8231cb887ee2170d96f1b570bb8b5
hellokack <eogeo85@gmail.com>
2026-05-11 15:51:49 +0900

    fix: 백엔드 서버 한글 깨짐(인코딩) 문제 해결

--- Week10/server.js
+++ Week10/server.js
@@ -7,7 +7,8 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('OK');
     } else {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        // 인코딩 설정(charset=utf-8) 추가
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end('Hello! AIOSS 10주차 컨테이너 배포 성공!');
     }
 });
